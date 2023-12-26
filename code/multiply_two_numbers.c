4705df76d2fbfbc14fffbda4daf2d13704da3511
Anton Louis Koetzler-Faust <antonkoetzler-faust@hotmail.com>
2023-12-26 14:12:53 -0300

    This branch is ready to be interactively rebased

---
+++ code/multiply_two_numbers.c
@@ -0,0 +1,1 @@
+int multiplyTwoNumbers(int x, int y) { return x * y; }
--- code/my_c_program.c
+++ code/my_c_program.c
@@ -4,10 +4,12 @@
 #include <stdio.h>
 #include "add_two_numbers.c"
 #include "subtract_two_numbers.c"
+#include "multiply_two_numbers.c"
 
 int main() {
   printf("This is my C++ program, please don't hack it :(\n");
   printf("This is the result of 1 + 2: %d\n", addTwoNumbers(1, 2));
   printf("This is the result of 1 - 2: %d\n", subtractTwoNumbers(1, 2));
+  printf("This is the result of 1 * 2: %d\n", multiplyTwoNumbers(1, 2));
   return 0;
 }
